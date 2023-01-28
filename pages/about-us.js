7efe04cd1957c47fce841e6357818d1fec6fe84a
Pobarul-islam <pobarul3@gmail.com>
2023-01-28 11:37:07 +0600

    events app start and some file added

---
+++ pages/about-us.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>About us Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam dolores doloremque perferendis facilis voluptatem quidem, qui nobis et, sunt laudantium dolore explicabo. Minus non amet laboriosam labore pariatur inventore, quas consequatur totam, a ex voluptate praesentium aperiam vero, asperiores vitae.</p>
+        </div>
+    );
+};
+
+export default Page;
---
+++ pages/events/[cat]/[id].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h2>Our Single Events</h2>
+        </div>
+    );
+};
+
+export default Page;
